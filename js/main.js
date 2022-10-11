4eba7658bef4175912c4e4a6c5a9625aef71497c
FalkeN67 <114016021+FalkeN67@users.noreply.github.com>
2022-10-11 20:57:05 +0200

    Update 4
    Poprawienie funkcji sprawdzających

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var pola = ['pole1', 'pole2', 'pole3', 'pole4', 'pole5', 'pole6', 'pole7', 'pole8', 'pole9']
 var tura = 0
+var koniec = false
 
 pola[0] = '-'
 pola[1] = '-'
@@ -12,7 +13,6 @@ pola[7] = '-'
 pola[8] = '-'
 
 function zamiana1() {
-	sprawdz_gre()
 	if (pola[0] == '-' && tura % 2 == 0) {
 		document.getElementById('trd1').innerHTML = '<div>X</div>'
 		pola[0] = 'X'
@@ -24,10 +24,10 @@ function zamiana1() {
 	} else {
 		alert('Miejsce jest już zajęte!')
 	}
+	sprawdz_gre()
 }
 
 function zamiana2() {
-	sprawdz_gre()
 	if (pola[1] == '-' && tura % 2 == 0) {
 		document.getElementById('trd2').innerHTML = '<div>X</div>'
 		pola[1] = 'X'
@@ -39,10 +39,10 @@ function zamiana2() {
 	} else {
 		alert('Miejsce jest już zajęte!')
 	}
+	sprawdz_gre()
 }
 
 function zamiana3() {
-	sprawdz_gre()
 	if (pola[2] == '-' && tura % 2 == 0) {
 		document.getElementById('trd3').innerHTML = '<div>X</div>'
 		pola[2] = 'X'
@@ -54,10 +54,10 @@ function zamiana3() {
 	} else {
 		alert('Miejsce jest już zajęte!')
 	}
+	sprawdz_gre()
 }
 
 function zamiana4() {
-	sprawdz_gre()
 	if (pola[3] == '-' && tura % 2 == 0) {
 		document.getElementById('trd4').innerHTML = '<div>X</div>'
 		pola[3] = 'X'
@@ -69,10 +69,10 @@ function zamiana4() {
 	} else {
 		alert('Miejsce jest już zajęte!')
 	}
+	sprawdz_gre()
 }
 
 function zamiana5() {
-	sprawdz_gre()
 	if (pola[4] == '-' && tura % 2 == 0) {
 		document.getElementById('trd5').innerHTML = '<div>X</div>'
 		pola[4] = 'X'
@@ -84,10 +84,10 @@ function zamiana5() {
 	} else {
 		alert('Miejsce jest już zajęte!')
 	}
+	sprawdz_gre()
 }
 
 function zamiana6() {
-	sprawdz_gre()
 	if (pola[5] == '-' && tura % 2 == 0) {
 		document.getElementById('trd6').innerHTML = '<div>X</div>'
 		pola[5] = 'X'
@@ -99,10 +99,10 @@ function zamiana6() {
 	} else {
 		alert('Miejsce jest już zajęte!')
 	}
+	sprawdz_gre()
 }
 
 function zamiana7() {
-	sprawdz_gre()
 	if (pola[6] == '-' && tura % 2 == 0) {
 		document.getElementById('trd7').innerHTML = '<div>X</div>'
 		pola[6] = 'X'
@@ -114,10 +114,10 @@ function zamiana7() {
 	} else {
 		alert('Miejsce jest już zajęte!')
 	}
+	sprawdz_gre()
 }
 
 function zamiana8() {
-	sprawdz_gre()
 	if (pola[7] == '-' && tura % 2 == 0) {
 		document.getElementById('trd8').innerHTML = '<div>X</div>'
 		pola[7] = 'X'
@@ -131,6 +131,7 @@ function zamiana8() {
 	} else {
 		alert('Miejsce jest już zajęte!')
 	}
+	sprawdz_gre()
 }
 
 function zamiana9() {
@@ -138,48 +139,59 @@ function zamiana9() {
 		document.getElementById('trd9').innerHTML = '<div>X</div>'
 		pola[8] = 'X'
 		tura++
-		sprawdz_gre()
 	} else if (pola[8] == '-' && tura % 2 == 1) {
 		document.getElementById('trd9').innerHTML = '<div>O</div>'
 		pola[8] = 'O'
 		tura++
-		sprawdz_gre()
 	} else {
 		alert('Miejsce jest już zajęte!')
 	}
+	sprawdz_gre()
 }
 
+/*Funkcja całkowita sprawdzająca koniec gry*/
 function sprawdz_gre() {
 	sprawdzenie_x()
 	sprawdzenie_y()
 	sprawdzenie_remis()
+	if (koniec == true) {
+		document.getElementById('plansza').innerHTML = '<div>Koniec</div>'
+	}
 }
 
 /*Sprawdzenie wygranej X*/
 function sprawdzenie_x() {
 	if (pola[0] == 'X' && pola[1] == 'X' && pola[2] == 'X') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[3] == 'X' && pola[4] == 'X' && pola[5] == 'X') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[6] == 'X' && pola[7] == 'X' && pola[8] == 'X') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[0] == 'X' && pola[3] == 'X' && pola[6] == 'X') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[1] == 'X' && pola[4] == 'X' && pola[7] == 'X') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[2] == 'X' && pola[5] == 'X' && pola[8] == 'X') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[0] == 'X' && pola[4] == 'X' && pola[8] == 'X') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[2] == 'X' && pola[4] == 'X' && pola[6] == 'X') {
 		alert('koniec gry1')
+		koniec = true
 	}
 }
 
@@ -187,27 +199,35 @@ function sprawdzenie_x() {
 function sprawdzenie_y() {
 	if (pola[0] == 'O' && pola[1] == 'O' && pola[2] == 'O') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[3] == 'O' && pola[4] == 'O' && pola[5] == 'O') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[6] == 'O' && pola[7] == 'O' && pola[8] == 'O') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[0] == 'O' && pola[3] == 'O' && pola[6] == 'O') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[1] == 'O' && pola[4] == 'O' && pola[7] == 'O') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[2] == 'O' && pola[5] == 'O' && pola[8] == 'O') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[0] == 'O' && pola[4] == 'O' && pola[8] == 'O') {
 		alert('koniec gry1')
+		koniec = true
 	}
 	if (pola[2] == 'O' && pola[4] == 'O' && pola[6] == 'O') {
 		alert('koniec gry1')
+		koniec = true
 	}
 }
 
