31c1b5a0da06f1623df05255000a1e5e6190abe1
FalkeN67 <114016021+FalkeN67@users.noreply.github.com>
2022-10-11 18:56:06 +0200

    Update
    Update 1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var pola = ['pole1', 'pole2', 'pole3', 'pole4', 'pole5', 'pole6', 'pole7', 'pole8', 'pole9']
+var tura = 0
+
 pola[0] = '-'
 pola[1] = '-'
 pola[2] = '-'
@@ -9,6 +11,16 @@ pola[6] = '-'
 pola[7] = '-'
 pola[8] = '-'
 
-function zamiana() {
-	document.getElementById('trd1').innerHTML = '<div>X</div>'
+function zamiana1() {
+	if (pola[0] == '-' && tura % 2 == 0) {
+		document.getElementById('trd1').innerHTML = '<div>X</div>'
+		pola[0] = 'X'
+		tura++
+	} else if (pola[0] == '-' && tura % 2 == 1) {
+		document.getElementById('trd1').innerHTML = '<div>O</div>'
+		pola[0] = 'O'
+		tura++
+	} else {
+		alert('Miejsce jest już zajęte!')
+	}
 }
