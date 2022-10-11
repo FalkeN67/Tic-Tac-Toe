113c6581c74ed7b0cf18bed35d84d1facfc28ed3
FalkeN67 <114016021+FalkeN67@users.noreply.github.com>
2022-10-11 17:17:31 +0200

    Add files
    Add files 1

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var pola = ['pole1', 'pole2', 'pole3', 'pole4', 'pole5', 'pole6', 'pole7', 'pole8', 'pole9']
+pola[0] = '-'
+pola[1] = '-'
+pola[2] = '-'
+pola[3] = '-'
+pola[4] = '-'
+pola[5] = '-'
+pola[6] = '-'
+pola[7] = '-'
+pola[8] = '-'
+
+function zamiana() {
+	document.getElementById('trd1').innerHTML = '<div>X</div>'
+}
